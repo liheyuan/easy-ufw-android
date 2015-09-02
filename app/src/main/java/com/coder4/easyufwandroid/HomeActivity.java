514558417a5ccff51e3b55a63ba085ae18940b23
lihy <lihy@fenbi.com>
2015-09-02 12:41:36 +0800

    drawer auto close when click

--- app/src/main/java/com/coder4/easyufwandroid/HomeActivity.java
+++ app/src/main/java/com/coder4/easyufwandroid/HomeActivity.java
@@ -2,6 +2,7 @@ package com.coder4.easyufwandroid;
 
 import android.app.usage.UsageEvents;
 import android.net.Uri;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,12 +14,20 @@ import com.coder4.easyufwandroid.fragment.NavigationDrawerFragment;
 
 import de.greenrobot.event.EventBus;
 
-public class HomeActivity extends AppCompatActivity implements NavigationDrawerFragment.OnFragmentInteractionListener {
+public class HomeActivity extends AppCompatActivity implements
+        NavigationDrawerFragment.OnFragmentInteractionListener {
+
+    NavigationDrawerFragment navigationDrawerFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        navigationDrawerFragment = (NavigationDrawerFragment)getSupportFragmentManager().findFragmentById(
+                R.id.navigation_drawer);
+        navigationDrawerFragment.setUpDrawer(R.id.navigation_drawer,
+                (DrawerLayout) findViewById(R.id.drawer_layout));
 
         // register event bus
         EventBus.getDefault().register(this);
@@ -37,7 +46,7 @@ public class HomeActivity extends AppCompatActivity implements NavigationDrawerF
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
+        // noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
@@ -53,7 +62,7 @@ public class HomeActivity extends AppCompatActivity implements NavigationDrawerF
         EventBus.getDefault().unregister(this);
     }
 
-    public void onEventMainThread(NavigationDrawerClickEvent event){
+    public void onEventMainThread(NavigationDrawerClickEvent event) {
         Toast.makeText(this, event.getPosition() + " clicked", Toast.LENGTH_LONG).show();
     }
 
--- app/src/main/java/com/coder4/easyufwandroid/fragment/NavigationDrawerFragment.java
+++ app/src/main/java/com/coder4/easyufwandroid/fragment/NavigationDrawerFragment.java
@@ -3,7 +3,9 @@ package com.coder4.easyufwandroid.fragment;
 import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,69 +18,70 @@ import com.coder4.easyufwandroid.event.NavigationDrawerClickEvent;
 
 import de.greenrobot.event.EventBus;
 
-
 /**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link NavigationDrawerFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link NavigationDrawerFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * A simple {@link Fragment} subclass. Activities that contain this fragment must implement the
+ * {@link NavigationDrawerFragment.OnFragmentInteractionListener} interface to handle interaction
+ * events. Use the {@link NavigationDrawerFragment#newInstance} factory method to create an instance
+ * of this fragment.
  */
 public class NavigationDrawerFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
+    private View containerView;
+
     private ListView drawerListView;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NavigationDrawerFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static NavigationDrawerFragment newInstance(String param1, String param2) {
-        NavigationDrawerFragment fragment = new NavigationDrawerFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
 
     public NavigationDrawerFragment() {
         // Required empty public constructor
     }
 
+    public void setUpDrawer(int fragmentId, DrawerLayout dl) {
+        // container view
+        containerView = getActivity().findViewById(fragmentId);
+        // drawer layout
+        drawerLayout = dl;
+
+        // set up drawer toggle
+        drawerToggle = new ActionBarDrawerToggle(getActivity(), drawerLayout, true,
+                R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close){
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                getActivity().invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getActivity().invalidateOptionsMenu();
+            }
+        };
+        drawerLayout.setDrawerListener(drawerToggle);
+
+    }
+
+    @SuppressWarnings("deprecation")
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+            Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        drawerListView = (ListView)inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
+        drawerListView = (ListView) inflater.inflate(R.layout.fragment_navigation_drawer,
+                container, false);
         drawerListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                EventBus.getDefault().post(new NavigationDrawerClickEvent(position));
+                selectItem(position);
             }
         });
         drawerListView.setAdapter(new NavigationDrawerAdapter(getActivity()));
@@ -86,11 +89,10 @@ public class NavigationDrawerFragment extends Fragment {
         return drawerListView;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+    private void selectItem(int position) {
+        EventBus.getDefault().post(new NavigationDrawerClickEvent(position));
+        // close drawer
+        drawerLayout.closeDrawers();
     }
 
     @Override
@@ -111,14 +113,13 @@ public class NavigationDrawerFragment extends Fragment {
     }
 
     /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
+     * This interface must be implemented by activities that contain this fragment to allow an
+     * interaction in this fragment to be communicated to the activity and potentially other
+     * fragments contained in that activity.
      * <p/>
      * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
+     * "http://developer.android.com/training/basics/fragments/communicating.html" >Communicating
+     * with Other Fragments</a> for more information.
      */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
